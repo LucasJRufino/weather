f61fe019dcb40913653f5b632ece9610e637d381
LucasJRufino <lucas.c.rufino@hotmail.com>
2024-02-28 21:41:50 -0300

    first commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "b20512bd6fcd870b5c48d154c84f8c38",
   base: "https://api.openweathermap.org/data/2.5/",
 };
